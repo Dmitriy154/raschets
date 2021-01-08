8527467fb97f3eddcf18b2d2660514b47ffd037a
Dmitriy154 <dmitriy154@gmail.com>
2021-01-08 16:59:50 +0300

    tp

--- dog/js/tp.js
+++ dog/js/tp.js
@@ -1,34 +1,8 @@
-b_tp.addEventListener('click', ()=> {
-    hideForm();
-    f_tp.style.display = "block";
-});
-
-b_ry.addEventListener('click', ()=> {
-    hideForm();
-    f_ry.style.display = "block";
-});
-
-
-
-function hideForm (){
-    let forms = document.querySelectorAll('form');
-    document.querySelectorAll('form')[0].style.display = "none";
-    document.querySelectorAll('form')[1].style.display = "none";
+function cr (_parent, _tagName, _class, _textContent) {
+    let elem = document.createElement(_tagName);
+    elem.className = _class;
+    elem.textContent = _textContent;
+    _parent.appendChild(elem);
+    return elem; 
 }
-
-btn_send_1.addEventListener('click', (e)=> {findBlank (e)});
-btn_send_2.addEventListener('click', (e)=> {findBlank (e)});
-
-function findBlank (e){
-    let kol = e.target.parentNode.length -1;
-
-    for (let i = 0; i < kol; i++) { 
-        if (e.target.parentNode[i].value == "") {
-            alert ("Есть незаполненные данные:  " + e.target.parentNode[i].name);
-            e.preventDefault(); //отменяем клик по умолчанию
-            break;
-        }
-    }
-}
-
 
