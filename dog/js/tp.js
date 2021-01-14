d48057a58597f7cee32c8d9d34694bbe887fbfdf
Dmitriy154 <dmitriy154@gmail.com>
2021-01-14 17:16:25 +0300

    manipula

--- dog/js/tp.js
+++ dog/js/tp.js
@@ -6,3 +6,70 @@ function cr (_parent, _tagName, _class, _textContent) {
     return elem; 
 }
 
+let _stage1 = cr(stage,'form'); //_stage1 - скрываемая часть (кроме кнопок) _stage2 - таблица и т.д.
+    let divForm = cr(_stage1, 'div', 'container-xl');
+        let row1 = cr(divForm, 'div', 'row'); 
+            
+            //адрес расчета
+            let div_address = cr(row1, 'div', 'col-sm-6 input-group m-1 p-0').innerHTML = `
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Адрес расчета</span>
+                </div>
+                <input type="text" class="form-control">
+            `;
+
+            //Дата расчета
+            let div_date = cr(row1, 'div', 'col-sm-2 m-1 p-0');
+                let _date = cr(div_date, 'input', 'form-control');
+                    _date.type = 'date';
+            
+            //расстояние до ПАСЧ
+            let div_rast = cr(row1, 'div', 'col-sm-2 m-1 p-0');
+                let _rast = cr(div_rast, 'input', 'form-control');
+                    _rast.setAttribute('placeholder', 'Расст. до ПАСЧ, км');
+            
+            //чек в населенном пункте
+            let _divNP = cr(row1, 'div', 'col mt-2 p-1');
+                let _divNP2 = cr(_divNP, 'div', 'custom-control custom-radio');
+                    _divNP2.innerHTML = `
+                        <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
+                        <label class="custom-control-label" for="customRadio1">в нас. пункте</label>
+                    `;
+       
+        let row2 = cr(divForm, 'div', 'row');
+            //Примечание
+            let div_note = cr(row2, 'div', 'form-group w-100 mt-1 mb-2');
+                let _note = cr(div_note, 'textarea', 'form-control');
+                _note.setAttribute('rows', '1');
+                _note.setAttribute('placeholder', 'Примечание для расчета (наличие забора, защита элементов)');
+
+
+//создание зданий tp_zd
+///////  создаем здание №1 ////////////////////
+let arrZd = [];
+let numZd = 1; //номер здания
+arrZd[0] = new Zd(1, _stage1);
+
+
+//строка для кнопок "Добавить здание" и "Продолжить расчет"
+let row_btns = cr(_stage1,'div','row justify-content-center');
+    let btn_addZd = cr(row_btns, 'button', 'btn btn-primary btn-sm m-2', 'Добавить здание');
+        btn_addZd.type = 'button';
+
+    let btn_next1 = cr(row_btns, 'button', 'btn btn-success btn-sm m-2', 'Продолжить расчет');
+        btn_next1.type = 'button'; 
+
+//обработчик для кнопки добавить здание
+btn_addZd.addEventListener('click', ()=> {
+    let newZd = new Zd(arrZd.length + 1, _stage1);
+    arrZd[arrZd.length] = newZd;
+
+    //вставляем здание перед кнопками
+    row_btns.before(newZd.row_container_zd);
+});
+
+//обработчик для кнопки продолжить расчет
+btn_next1.addEventListener('click', ()=> {
+    //скрываем первый кадр, сцену
+    _stage1.style = "display:none";
+});
---
+++ dog/js/tp_zd.js
@@ -0,0 +1,67 @@
+//создание зданий, исходные данные
+class Zd {
+    tip; //тип здания (дом, сарай, гараж ....)
+    address;
+    walls; //материал стен, отделка
+    h; // высота здания
+    hk; // высота крыши
+    info; //дополнительная информация (размеры в плане)
+    row_container_zd; //ссылка на контейнер участка
+
+constructor(num, _stage) {
+    this.num = num; //номер здания
+
+    let divZd = cr(_stage, 'div', 'container-lg p-0');
+
+        this.row_container_zd = divZd; //свойство ссылается на контейнер для каждого здания
+
+        let row_zd = cr(divZd,'div','row m-0 p-0 justify-content-center border border-primary');
+            let col_zd = cr(row_zd, 'div', 'container m-0 p-0');
+
+                let row_zd_1 = cr(col_zd, 'div', 'row form-group row m-1 p-1 justify-content-center');
+                    
+                    //номер здания
+                    let divNum = cr(row_zd_1, 'div', 'col-sm-1 m-auto p-0');
+                        let _num = cr(divNum, 'h6');
+                        _num.textContent = 'Здание №'+num;
+
+                    //вид здания
+                    let divTip = cr(row_zd_1, 'div', 'col-sm-2 m-1 p-0');
+                        let _tip = cr(divTip, 'input', 'form-control');
+                        _tip.setAttribute('placeholder', 'вид здания');
+                            this.tip = _tip;
+               
+                    //адрес
+                    let divAdr = cr(row_zd_1, 'div', 'col-sm-6 m-1 p-0');
+                        let _adr = cr(divAdr, 'input', 'form-control');
+                        _adr.setAttribute('placeholder', 'Адрес здания');
+                            this.address = _adr;
+
+                    //материал стен
+                    let divMat = cr(row_zd_1, 'div', 'col-sm-2 m-1 p-0');
+                        let _mat = cr(divMat, 'input', 'form-control');
+                        _mat.setAttribute('placeholder', 'Материал стен');
+                            this.walls = _mat;                           
+
+
+                let row_zd_2 = cr(col_zd, 'div', 'row form-group row m-0 p-0 justify-content-center');
+                    //высота здания
+                    let divH = cr(row_zd_2, 'div', 'col-sm-1 p-1');
+                        let _H = cr(divH, 'input', 'form-control');
+                        _H.setAttribute('placeholder', 'h здания');
+                            this.h = _H;                   
+                    
+                    //высота крыши здания
+                    let divHk = cr(row_zd_2, 'div', 'col-sm-1 p-1');
+                        let _Hk = cr(divHk, 'input', 'form-control');
+                        _Hk.setAttribute('placeholder', 'h крыши');
+                            this.hk = _Hk;   
+                    
+                    //Причечание дял здания
+                    let divInf = cr(row_zd_2, 'div', 'col-sm-10 p-1');
+                        let _Inf = cr(divInf, 'input', 'form-control');
+                        _Inf.setAttribute('placeholder', 'Примечание для здания');
+                            this.info = _Inf;               
+
+}//конструктор
+}//class
